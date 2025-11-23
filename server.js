5eaa301db59489b5d142566632e9e111878bb312
keshav <keshavbsk7@gmail.com>
2025-11-23 23:49:37 +0530

    Updated port logic for Render deployment

--- server.js
+++ server.js
@@ -134,4 +134,6 @@ app.post("/login", async (req, res) => {
 
 // ============================================
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
