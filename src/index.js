2ecbfd572fe4e97f29e8766750055a8a6a95c7a5
keshavbsk7 <keshavbsk7@gmail.com>
2026-01-03 14:26:37 +0530

    nodemailer changes

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Root from "./Root";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Root />);
